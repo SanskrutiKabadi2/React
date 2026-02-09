a2ea0bc3cf152e24d5f7360a6a3859d5b53307aa
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2026-02-09 13:16:32 +0530

    Create app.jsx

---
+++ Prop Drilling/app.jsx
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Dashboard from './dashboard'
+
+function App() {
+  return (
+    <Dashboard userName={"sanskruti"}/>
+  );
+}
+
+export default App;
